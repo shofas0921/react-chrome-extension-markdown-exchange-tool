335f6273dbd428c42151c06220d1bb7a3d2ba38e
shofas0921 <motohashi.sho@gmail.com>
2024-05-12 17:34:01 +0900

    [Update]create GoogleChrome environment

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "CRX Example",
+  version: "1.0.0",
+  action: {
+    default_popup: "index.html",
+  },
+});
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+});
